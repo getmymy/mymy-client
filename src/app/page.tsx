bdff3e56cf43ea9815b0350fc30fe54dcfd13d8e
nyeoni <skdusdl8804@gmail.com>
2023-11-11 18:41:34 +0900

    feat: Button, Input 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 
+import { Button, Input } from '@/components/ui';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Razzle
       <Link href="/login">로그인</Link>
+      <Button>버튼</Button>
+      <Input placeholder="값을 입력해주세요" />
     </main>
   );
 }
