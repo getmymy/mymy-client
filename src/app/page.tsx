0a60dd036dbfcaf65f371b37a00ae60d91d46a1f
Yenowme <76904042+Yenowme@users.noreply.github.com>
2023-11-25 20:06:04 +0900

    [T-194] 기본 라우팅 셋업 (#3)
    * feat: 기본 라우팅 설정
    
    * feat: razzle에서 mymy로 네이밍 변경
    
    * fix: signup -> users/singup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { Button, Input } from '@/components/ui';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Razzle
-      <Link href="/login">로그인</Link>
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      Mymy
+      <Link href="/sign-in">가입</Link>
       <Button>버튼</Button>
       <Input placeholder="값을 입력해주세요" />
     </main>
