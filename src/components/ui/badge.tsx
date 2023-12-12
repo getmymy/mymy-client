03a0bbc8a81bc69807a48ead1bdce9efd80da772
nyeoni <skdusdl8804@gmail.com>
2023-12-12 21:30:46 +0900

    feat: Badge 컴포넌트 구현

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+const badgeVariants = cva('inline-flex items-center rounded-full px-[0.75rem] py-[0.4375rem] gap-[0.25rem]', {
+  variants: {
+    variant: {
+      default: 'border-transparent bg-white-a10 text-[0.8125rem]',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
+export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {
+  label: string;
+  number?: number;
+}
+
+function Badge({ className, variant, label, number, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props}>
+      <span className="text-white-a80">{label}</span>
+      {number && <span className="text-white-a100">{number}</span>}
+    </div>
+  );
+}
+
+export { Badge, badgeVariants };
