445debaa8ec39f307c3564cbebf26078293f37f3
yenowme <sag03137@gmail.com>
2023-12-14 18:37:56 +0900

    feat: bottomsheet에 youtubeplayer 추가

--- src/app/search/components/BottomSheet.tsx
+++ src/app/search/components/BottomSheet.tsx
@@ -1,28 +1,45 @@
+'use client';
+
+import { useRef } from 'react';
+import { YouTubeEvent } from 'react-youtube';
+
 import { Button } from '@/components/ui';
 
 import Cd from './Cd';
 import { Music } from './ListItem';
+import YoutubePlayer from './YoutubePlayer';
 
 export default function BottomSheet({ selectMusic }: { selectMusic: Music }) {
+  const eventRef = useRef<YouTubeEvent>(null);
+
+  const onClick = () => {
+    eventRef.current?.target.playVideo();
+  };
+
   return (
-    <div className="flex-shrink-0 pt-5 px-5 w-full rounded-t-[30px] bg-white-a10 absolute bottom-0">
-      <button className=" text-white  font-medium leading-[140%]">취소</button>
+    <>
+      <div className="flex-shrink-0 pt-5 px-5 w-full rounded-t-[30px] bg-white-a10 absolute bottom-0 z-10 backdrop-blur-md">
+        <button onClick={onClick} className=" text-white  font-medium leading-[140%]">
+          취소
+        </button>
 
-      <Cd src={selectMusic.thumbnailUrl} />
+        <Cd src={selectMusic.thumbnailUrl} />
 
-      <div className="flex flex-col items-center gap-4 w-full">
-        <div className="flex flex-col items-start gap-1 self-stretch">
-          <div className="text-white text-2xl font-bold leading-[140%]">{selectMusic.musicTitle}</div>
+        <div className="flex flex-col items-center gap-4 w-full">
+          <div className="flex flex-col items-start gap-1 self-stretch">
+            <div className="text-white text-2xl font-bold leading-[140%]">{selectMusic.musicTitle}</div>
 
-          <div className="flex items-start gap-1">
-            <div className="text-white-a60 font-medium leading-[140%]">{selectMusic.artist}</div>
-            <div className="text-white-a20 text-center text-xs font-medium leading-[140%]">|</div>
-            <div className="text-white-a60 font-medium leading-[140%]">{selectMusic.albumTitle}</div>
+            <div className="flex items-start gap-1">
+              <div className="text-white-a60 font-medium leading-[140%]">{selectMusic.artist}</div>
+              <div className="text-white-a20 text-center text-xs font-medium leading-[140%]">|</div>
+              <div className="text-white-a60 font-medium leading-[140%]">{selectMusic.albumTitle}</div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <Button className="my-[1.25rem]">Share</Button>
-    </div>
+        <Button className="my-[1.25rem]">Share</Button>
+      </div>
+      <YoutubePlayer ref={eventRef} youtubeId={selectMusic.musicId} />
+    </>
   );
 }
