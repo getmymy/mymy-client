d16e118292aa2f939848460cb9c65b2a307b5c88
yenowme <sag03137@gmail.com>
2024-01-01 18:38:47 +0900

    feat: cd 컴포넌트 분리

--- src/app/search/components/Cd.tsx
+++ src/app/search/components/Cd.tsx
@@ -1,12 +1,39 @@
+import Image from 'next/image';
+
 export default function Cd({ src }: { src: string }) {
   return (
-    <div className="flex justify-center items-center mb-4">
-      <div className="absolute flex items-center justify-center">
-        <svg width={48} height={48} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <circle cx={24} cy={24} r={24} fill="#101012" />
-        </svg>
+    <>
+      <svg
+        className="absolute"
+        width="232"
+        height="232"
+        viewBox="0 0 232 232"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <mask id="mask">
+          <path
+            opacity="0.9"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M116 232C180.065 232 232 180.065 232 116C232 51.935 180.065 0 116 0C51.935 0 0 51.935 0 116C0 180.065 51.935 232 116 232ZM117 145C132.464 145 145 132.464 145 117C145 101.536 132.464 89 117 89C101.536 89 89 101.536 89 117C89 132.464 101.536 145 117 145Z"
+            fill="#FFFFFF"
+          />
+        </mask>
+      </svg>
+
+      <div className="flex justify-center items-center mb-4">
+        <Image
+          alt=""
+          width={232}
+          height={232}
+          style={{
+            maskImage: 'url(#mask)',
+            WebkitMaskImage: 'url(#mask)',
+          }}
+          src={src}
+        />
       </div>
-      <img className="flex-shrink-0 w-52 h-52 border rounded-full border-white-a20" src={src} />
-    </div>
+    </>
   );
 }
