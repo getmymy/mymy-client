f00d5eb54642dadcc7b9fda711f5d107db489c4e
yenowme <sag03137@gmail.com>
2024-01-01 18:35:58 +0900

    feat: img -> next Image tag

--- src/app/search/components/ListItem.tsx
+++ src/app/search/components/ListItem.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import Image from 'next/image';
+
 export interface Music {
   id: string;
   musicTitle: string;
@@ -5,23 +9,32 @@ export interface Music {
   albumTitle: string;
   thumbnailUrl: string;
   audioUrl: string;
+  musicId: string;
 }
 export default function ListItem({ music }: { music: Music }) {
   return (
-    <button className="flex items-center gap-4 py-2  w-full">
-      <img alt={''} src={music.thumbnailUrl} className="w-16 h-16 border border-[#ffffff]/[.20] rounded-full" />
-      <div className="flex flex-col items-start gap-0.5">
-        <div className="text-white-100  text-lg font-semibold leading-[140%]">{music.musicTitle}</div>
-        <div className="flex items-start gap-1">
-          <div className="text-white-a40  text-[.8125rem] font-medium leading-[140%] whitespace-nowrap">
-            {music.artist}
+    <>
+      <div className="flex items-center gap-4 py-2  w-full">
+        <Image
+          alt={''}
+          width={64}
+          height={64}
+          src={music.thumbnailUrl}
+          className="border border-[#ffffff]/[.20] rounded-full"
+        />
+        <button className="flex flex-col items-start gap-0.5">
+          <div className="text-white-100  text-lg font-semibold leading-[140%]">{music.musicTitle}</div>
+          <div className="flex items-start gap-1">
+            <div className="text-white-a40  text-[.8125rem] font-medium leading-[140%] whitespace-nowrap">
+              {music.artist}
+            </div>
+            <div className="text-white-a20 text-white text-center  text-xs font-medium leading-[140%]">|</div>
+            <div className="text-white-a40 text-[.8125rem] font-medium leading-[140%] line-clamp-1 break-all">
+              {music.albumTitle}
+            </div>
           </div>
-          <div className="text-white-a20 text-white text-center  text-xs font-medium leading-[140%]">|</div>
-          <div className="text-white-a40 text-[.8125rem] font-medium leading-[140%] line-clamp-1 break-all">
-            {music.albumTitle}
-          </div>
-        </div>
+        </button>
       </div>
-    </button>
+    </>
   );
 }
