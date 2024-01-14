91c75b191c1a41085e0b5148880c75482ae1e7d3
yenowme <sag03137@gmail.com>
2024-01-14 17:03:36 +0900

    feat: 바텀시트 애니메이션시 스크롤바 제거

--- src/app/search/components/BottomSheet.tsx
+++ src/app/search/components/BottomSheet.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRef } from 'react';
+import { createPortal } from 'react-dom';
 import { YouTubeEvent } from 'react-youtube';
 
 import { AnimatePresence, motion } from 'framer-motion';
@@ -18,19 +19,19 @@ export default function BottomSheet({ selectMusic, onClose }: { selectMusic: Mus
     eventRef.current?.target.playVideo();
   };
 
-  return (
+  return createPortal(
     <AnimatePresence>
       {selectMusic && (
-        <>
+        <div className="absolute h-full w-full top-0 overflow-hidden">
           {/* 백그라운드 컴포넌트  */}
-          <div className="h-full w-full absolute top-0 z-0" onClick={onClose}></div>
+          <div className="h-full w-full absolute top-0" onClick={onClose}></div>
 
           <motion.div
             key="modal"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 50 }}
-            className="flex-shrink-0 pt-5 px-5 w-full rounded-t-[30px] bg-white-a10 absolute bottom-0 z-10 backdrop-blur-md"
+            className="flex-shrink-0 pt-5 px-5 w-full rounded-t-[30px] bg-white-a10 absolute bottom-0 backdrop-blur-md"
           >
             <button onClick={onClick} className=" text-white  font-medium leading-[140%]">
               취소
@@ -38,6 +39,7 @@ export default function BottomSheet({ selectMusic, onClose }: { selectMusic: Mus
 
             <Cd src={selectMusic.thumbnailUrl} />
 
+            {/* @todo: 타이포 컴포넌트로 변경 */}
             <div className="flex flex-col items-center gap-4 w-full">
               <div className="flex flex-col items-start gap-1 self-stretch">
                 <div className="text-white text-2xl font-bold leading-[140%]">{selectMusic.musicTitle}</div>
@@ -53,8 +55,9 @@ export default function BottomSheet({ selectMusic, onClose }: { selectMusic: Mus
             <Button className="my-[1.25rem]">Share</Button>
           </motion.div>
           <YoutubePlayer ref={eventRef} youtubeId={selectMusic.musicId} />
-        </>
+        </div>
       )}
-    </AnimatePresence>
+    </AnimatePresence>,
+    document.body,
   );
 }
