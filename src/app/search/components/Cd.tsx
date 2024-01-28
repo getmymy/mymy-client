07db791de34c89d17b971bf1b57c7734096d0a82
yenowme <sag03137@gmail.com>
2024-01-28 16:19:58 +0900

    fix: fill-rule & clip-rule 카멜 케이스로 수정

--- src/app/search/components/Cd.tsx
+++ src/app/search/components/Cd.tsx
@@ -14,8 +14,8 @@ export default function Cd({ src }: { src: string }) {
         <mask id="mask">
           <path
             opacity="0.9"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M116 232C180.065 232 232 180.065 232 116C232 51.935 180.065 0 116 0C51.935 0 0 51.935 0 116C0 180.065 51.935 232 116 232ZM117 145C132.464 145 145 132.464 145 117C145 101.536 132.464 89 117 89C101.536 89 89 101.536 89 117C89 132.464 101.536 145 117 145Z"
             fill="#FFFFFF"
           />
